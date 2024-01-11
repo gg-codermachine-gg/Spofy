c804ba1fc0e191bf57c97ba3b2be577e963abcea
gg-codermachine-gg <119689044+gg-codermachine-gg@users.noreply.github.com>
2024-01-11 20:15:12 +0200

    nothing new just minor updates didnt really had the time to work on something new

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SideBar from "@/components/SideBar";
 import MusicPlayerStatus from "@/components/MusicStatus";
+import PlaylistCardSmall from "@/components/cards/PlaylistCardSmall";
 
 export default function Page() {
   return (
@@ -13,7 +14,7 @@ export default function Page() {
 }
 const MainBody: React.FC = () => {
   return <div className="w-[90%] h-[100vh] bg-gradient-to-b from-neutral-800 via-stone-900 to-neutral-900"> 
-  <div>23</div>
+  <div>PlaylistCardSmall</div>
   </div>;
 }; 
 
